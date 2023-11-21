0d9cb7ad32fb6e575dc8e565579d5db4fb7c6022
emminyell <140811695+emminyell@users.noreply.github.com>
2023-11-21 14:03:12 +0000

    aligner les lignes

--- data.sql
+++ data.sql
@@ -6,14 +6,14 @@ creat database you contact
     user_name varchar(255),
     mot_passe varchar(255)
     );
-  CREATE TABLE contact(
-   	 contact_id INT PRIMARY KEY AUTO_INCREMENT,
-   	 first_name varchar(255),
-     last_name varchar(255),
-     email varchar(255),
-     téléphone varchar(255),
-     description varchar(255),
-     date_creation date,
-     user_id int,
-     FOREIGN KEY (user_id) REFERENCES user_compte (user_id)
+    CREATE TABLE contact(
+        contact_id INT PRIMARY KEY AUTO_INCREMENT,
+        first_name varchar(255),
+        last_name varchar(255),
+        email varchar(255),
+        téléphone varchar(255),
+        description varchar(255),
+        date_creation date,
+        user_id int,
+        FOREIGN KEY (user_id) REFERENCES user_compte (user_id)
     );
