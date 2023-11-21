afd11f4b53bbf5c312dc9acd414a95fc686993f9
emminyell <140811695+emminyell@users.noreply.github.com>
2023-11-21 12:31:36 +0000

    ajouter database

---
+++ data.sql
@@ -0,0 +1,19 @@
+--- creat database 
+creat database you contact 
+
+	CREATE TABLE user_compte(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name varchar(255),
+    mot_passe varchar(255)
+    );
+  CREATE TABLE contact(
+   	 contact_id INT PRIMARY KEY AUTO_INCREMENT,
+   	 first_name varchar(255),
+     last_name varchar(255),
+     email varchar(255),
+     téléphone varchar(255),
+     description varchar(255),
+     date_creation date,
+     user_id int,
+     FOREIGN KEY (user_id) REFERENCES user_compte (user_id)
+    );
